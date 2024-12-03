f5bd842cf35355e44353c5ec93499c95c75431af
arielmz12344 <arielmz12344@gmail.com>
2024-12-03 02:48:04 +0200

    completed with comments until functions

--- ex3.c
+++ ex3.c
@@ -30,10 +30,12 @@ void printCube(int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES],int day);
 void getBestSoldBrandOverall(int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES],int day, int *bestBrand,int *totalBrandSum);
 void getBestSoldTypeOverall(int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES],int day, int *bestType, int *TotalTypeSum);
 void getBestSoldDayOverall(int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES],int day, int *bestDay, int *totalDaySum);
-void getADM(int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES], int day);
+void getAdm(int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES], int day);
 int main() {
     int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES];
+    //dayCounter will hold the latest day that is available for getting data from the user
     int dayCounter = 0;
+    //First I Want to initialize the cube, so I run on every index in the cube and initialize it to -1 as requested
     for (int i = 0; i < DAYS_IN_YEAR; i++) {
         for (int j = 0; j < NUM_OF_BRANDS; j++) {
             for (int k = 0; k < NUM_OF_TYPES; k++) {
@@ -42,40 +44,61 @@ int main() {
         }
     }
     int choice;
+    //choice will hold the choice the user inputs
     printMenu();
     scanf("%d", &choice);
+    //I want the program to continue until the user inputs 7(done), so this is the while loop condition
     while(choice != done){
         switch(choice) {
             case addOne: {
+                //In this case I only want to add one car brand sales for the latest day which is the value dayCounter holds
                 addData(cube,dayCounter);
                 break;
             }
             case addAll: {
-                int index = 0;
+                int counter = 0;
+                /*isAssigned array is an array with the size of the NUM_OF_BRANDS that will help me later to understand
+                 if a brand received an input from the user*/
                 int isAssigned[NUM_OF_BRANDS] = {-1,-1,-1,-1,-1};
+                //The initial print for the no data for brands and then a loop that prints all the brand names as requested
                 printf("No data for brands ");
                 for (int i = 0; i < NUM_OF_BRANDS; i++) {
                     printf("%s ", brands[i]);
                 }
                 printf("\n");
-                while(index < NUM_OF_BRANDS) {
+                /*This while loop is the loop in which we enter values to the available day for all brand,
+                 the only way we want to leave the loop is if all 5 brands got values,
+                 counter will only grow if a brand with no value got a new value so when all brands got values that means counter got to NUM_OF_BRANDS,
+                 and we can exit the loop
+                 */
+                while(counter < NUM_OF_BRANDS) {
+                    //isFound checks if a brand already got values, if so isFound is true(1), isFound is initialed at 0
                     int isFound = 0;
+                    /*Here I call addData on the cube and the available day,
+                     *and carBrandVal received the output of the function which is the brand number of the brand that got input values*/
                     int carBrandVal = addData(cube,dayCounter);
+                    // In this loop I want to check if a brand is already assigned if so isFound is true(1)
                     for (int i = 0; i < NUM_OF_BRANDS; i++) {
                         if (carBrandVal == isAssigned[i]) {
                             isFound = 1;
                             break;
                         }
                     }
+                    //If the brand already got values in the array I want to continue the loop as if nothing happened and enter those values instead of the old ones
                     if (!isFound) {
                         isAssigned[carBrandVal] = carBrandVal;
-                        index++;
+                        counter++;
                     }
-                    if ( index == NUM_OF_BRANDS) {
+                    /*If counter grew to NUM_OF_BRANDS that means the last brand got inputs
+                     and no need to continue the rest of the loop that prints more lines,
+                     only when were done and all the brands got inputs for that day we can add 1 to the dayCounter*/
+                    if (counter == NUM_OF_BRANDS) {
                         dayCounter++;
                         break;
                     }
                     printf("No data for brands ");
+                    /*Here we want to print again all the brands but exclude the brands that already got their inputs
+                     so we print only the brand names that their index wasn't changed in the isAssigned array*/
                     for (int i = 0; i < NUM_OF_BRANDS; i++) {
                         if (isAssigned[i] == -1) {
                             printf("%s ", brands[i]);
@@ -86,14 +109,24 @@ int main() {
                 break;
             }
             case stats: {
+                //checkDay will hold the day we want to check his stats
                 int checkDay;
+                //isDayValid will hold true(1) if the day that was input is a day that got values and false(0) if not
                 int isDayValid = 1;
                 printf("What day would you like to analyze?\n");
                 scanf("%d", &checkDay);
+                //In my code day 1 means day 0, so I subtract 1 from the checkDay
                 checkDay--;
+                /*Here I want to check if the checkDay is invalid,
+                 if the day is bigger then the last available day(dayCounter)-1,
+                 that means that there are no values that day and the checkDay is invalid, if the user input 0 as the check day (meaning -1 because of the subtraction)
+                 that's also invalid*/
                 if (checkDay > dayCounter-1 || checkDay == -1) {
                     isDayValid = 0;
                 }
+                /*So this loop continues to run until the user inputs a valid day,
+                 inside this loop we preform the inverse checks as before but now we check if the checkDay is valid,
+                 so when it is we change the isDayValid flag to true and leave the loop*/
                 while (!isDayValid) {
                     printf("Please enter a valid day.\nWhat day would you like to analyze?\n");
                     scanf("%d", &checkDay);
@@ -103,11 +136,19 @@ int main() {
                     }
 
                 }
+                //salesSum will be changed in the getTotalSalesSum function to the sum of all sales in the checkDay day and is now initialized to 0
                 int salesSum = 0;
+                //bestBrand will be changed in the getBestSoldBrand function to the index of the highest grossing brand in the checkDay day and is now initialized to -1
                 int bestBrand =-1;
+                //brandSum will be changed in the getBestSoldBrand function to the highest grossing brand sales sum in the checkDay day and is now initialized to 0
                 int brandSum = 0;
+                //bestType will be changed in the getBestSoldType function to the index of the highest grossing type in the checkDay day and is now initialized to -1
                 int bestType = -1;
+                //typeSum will be changed in the getBestSoldType function to the highest grossing type sales sum in the checkDay day and is now initialized to 0
                 int typeSum = 0;
+                /*In the following lines of code we print the message as requested
+                 and call the functions with the arguments cube and the checkDay so the function will now what to print and at what day,
+                 the other arguments each function get are the pointers of each variable we want to change so we can change him in the function*/
                 printf("In day number %d:\n", checkDay+1);
                 getTotalSalesSum(cube,checkDay,&salesSum);
                 printf("The sales total was %d\n", salesSum);
@@ -119,18 +160,29 @@ int main() {
                 break;
             }
             case print: {
+                /*Here we print the lines as requested and then call the function printCube
+                 with the arguments cube and the dayCounter so the function will now what to print and until what day*/
                 printf("*****************************************\n\n");
                 printCube(cube,dayCounter);
                 printf("\n\n*****************************************\n");
                 break;
             }
             case insights: {
-                int bestBrand;
-                int totalBrandSum;
-                int bestType;
-                int totalTypeSum;
-                int bestDay;
-                int totalDaySum;
+                //bestBrand will be changed in the getBestSoldBrandOverall function to the index of the highest grossing brand overall and is initialized to -1
+                int bestBrand = -1;
+                //totalBrandSum will be changed in the getBestSoldBrandOverall function to the sales sum of the  highest grossing brand overall and is initialized to 0
+                int totalBrandSum = 0;
+                //bestType will be changed in the getBestSoldTypeOverall function to the index of the highest grossing type overall and is initialized to -1
+                int bestType = -1;
+                //totalTypeSum will be changed in the getBestSoldTypeOverall function to the sales sum of the  highest grossing type overall and is initialized to 0
+                int totalTypeSum = 0;
+                //bestDay will be changed in the getBestSoldDayOverall function to the highest grossing day  and is initialized to -1
+                int bestDay = -1;
+                //totalDaySum will be changed in the getBestSoldDayOverall function to the sales sum of the highest grossing day  and is initialized to 0
+                int totalDaySum = 0;
+                /*In the following lines of code we print the message as requested
+                and call the functions with the arguments cube and the dayCounter so the function will now what to print and until which day,
+                the other arguments each function get are the pointers of each variable we want to change so we can change him in the function*/
                 getBestSoldBrandOverall(cube,dayCounter,&bestBrand,&totalBrandSum);
                 printf("The best-selling brand overall is %s: %d$ \n",brands[bestBrand],totalBrandSum);
                 getBestSoldTypeOverall(cube,dayCounter,&bestType,&totalTypeSum);
@@ -140,7 +192,8 @@ int main() {
                 break;
             }
             case deltas: {
-                getADM(cube,dayCounter);
+                //We call the getAdm function with the arguments cube and dayCounter so the function will now what to check and until which day
+                getAdm(cube,dayCounter);
                 break;
             }
             default:
@@ -296,7 +349,7 @@ void getBestSoldDayOverall(int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES],i
     }
    *totalDaySum = totalSumArr[*bestDay];
 }
-void getADM(int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES], int day){
+void getAdm(int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES], int day){
     if (day>1) {
         for (int i = 0; i < NUM_OF_BRANDS; i++) {
             float delta = 0;
